f3fbfa93fe6758f411fefb05f1ccc97e7bf32322
Sean <swagner7764@gmail.com>
2024-02-15 13:02:29 -0700

    chnges to main

--- DotnetConsoleApp/Program.cs
+++ DotnetConsoleApp/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!!!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!!!");
+
+        Console.WriteLine("Sleep for 10 seconds.");
+        Thread.Sleep(10000);
+
+        Console.WriteLine("Sleep completed.");
+    }
+}
