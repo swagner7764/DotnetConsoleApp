b7790224aa9d801d332f9da6348fe975868ba903
Sean Wagner <swagner7764@users.noreply.github.com>
2024-02-29 11:20:29 -0700

    Update Program.cs

--- DotnetConsoleApp/Program.cs
+++ DotnetConsoleApp/Program.cs
@@ -2,7 +2,7 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!!!!!!!!!!!");
+        Console.WriteLine("Hello, World!");
 
         Console.WriteLine("Sleep for 10 seconds.");
         Thread.Sleep(10000);
